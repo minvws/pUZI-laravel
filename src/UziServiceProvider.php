18aa2c08f3a36c9ccef293325c6b58a5efa68b37
Rick Lambrechts <rick@rl-webdiensten.nl>
2023-02-23 01:20:12 +0100

    Get only certificate part between start and end

--- src/UziServiceProvider.php
+++ src/UziServiceProvider.php
@@ -17,48 +17,56 @@ class UziServiceProvider extends ServiceProvider
      * Boots the Service provider.
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->publishes([
             __DIR__ . '/config/uzi.php' => config_path('uzi.php')
         ]);
     }
 
-    public function register()
+    public function register(): void
     {
         $this->app->bind(UziValidator::class, function () {
-
-            // Split certificates from file
-            $caCerts = [];
-            $path = config('uzi.ca_certs_path', null);
-            if ($path) {
-                $fileContent = @file_get_contents($path);
-                if ($fileContent === false) {
-                    throw new \RuntimeException("Could not read CA certificates from $path");
-                }
-
-                $caCerts = preg_split('/-----BEGIN CERTIFICATE-----/', $fileContent);
-                if ($caCerts === false) {
-                    $caCerts = [];
-                } else {
-                    // remove empty first element
-                    array_shift($caCerts);
-                }
-
-                foreach ($caCerts as &$cert) {
-                    $cert = trim($cert);
-                    $cert = str_replace('-----END CERTIFICATE-----', '', $cert);
-                    $cert = str_replace("\n", '', $cert);
-                }
-            }
-
             return new UziValidator(
                 new UziReader(),
                 config("uzi.strict_ca_check", true),
                 config("uzi.allowed_types", []),
                 config("uzi.allowed_roles", []),
-                $caCerts
+                $this->getCACerts(config("uzi.ca_cert_path")),
             );
         });
+    }
+
+    /**
+     * Get the CA certificates from the given path
+     * @param ?string $path
+     * @return array
+     */
+    public function getCACerts(?string $path): array
+    {
+        if (empty($path)) {
+            return [];
+        }
+
+        $fileContent = @file_get_contents($path);
+        if ($fileContent === false) {
+            throw new \RuntimeException("Could not read CA certificates from $path");
+        }
+
+        $caCerts = preg_split('/-----BEGIN CERTIFICATE-----/', $fileContent);
+        if ($caCerts === false) {
+            return [];
+        }
+
+        // remove empty first element
+        array_shift($caCerts);
+
+        foreach ($caCerts as &$cert) {
+            $cert = trim($cert);
+            $cert = substr($cert, 0, strpos($cert, '-----END CERTIFICATE-----') ?: 0);
+            $cert = str_replace("\n", '', $cert);
+        }
+
+        return $caCerts;
     }
 }
